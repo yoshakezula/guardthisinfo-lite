6e32812ffe4dc1992ff570d3b54dd583a0e70204
Kenny Chan <kenny.r.chan@gmail.com>
2013-08-11 16:56:11 -0400

    rm unnecessary components from bootstrap.js and bootstrap.css

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var require = {
     jquery: "libs/jquery-2.0.3.min",
     app: 'app',
     modernizr: 'libs/modernizr.custom.min',
-    bootstrap: 'libs/bootstrap.min'
+    bootstrap: 'libs/bootstrap-custom'
   },
 
   shim: {
